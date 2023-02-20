7321c3a77fa2047eaa3bb96ee8bf29c57316385e
thibault-cne <thibault.chene23@gmail.com>
2023-02-20 11:23:09 +0100

    [+] fix root style and continue styling lesson page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import First from './pages/lessons/first/first'
+import First from './pages/lessons/first/First'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
--- frontend/src/pages/lessons/first/First.tsx
+++ frontend/src/pages/lessons/first/First.tsx
@@ -1,7 +1,7 @@
 function First() {
     return (
         <div>
-            <h2>First lesson</h2>
+            <h2 className="p-4 font-semibold text-xl">First lesson</h2>
             <div>
                 <p>Minim laborum reprehenderit eu ex amet reprehenderit voluptate in ad enim magna ad veniam. Culpa officia elit proident laboris mollit excepteur pariatur sit. Aliqua officia minim consectetur nostrud do do enim irure sint. In aute fugiat tempor sunt occaecat nostrud sint. Non laboris in consequat ad nulla ad commodo irure consequat deserunt. Mollit dolore sunt pariatur proident.</p>
                 <p>Ad ullamco dolore enim ea labore labore qui eu occaecat magna aliqua. Nulla adipisicing deserunt culpa consequat consequat aliqua eiusmod dolor anim occaecat elit voluptate. Ea dolor enim incididunt eiusmod tempor dolore reprehenderit nisi exercitation excepteur sunt esse labore. Voluptate ullamco pariatur do aute non ex laborum deserunt dolore enim laborum esse.</p>
