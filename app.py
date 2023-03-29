d49aaebd34262fd3779eb520a1021934c176d0c3
Aurélien <aurelien.troncy@telecomnancy.eu>
2023-03-29 22:50:39 +0200

    [+] ajout de 4 lessons

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/lesson/<f>')
+def hello(f):
+    return render_template('lessons/lesson' +f+ '.html')
