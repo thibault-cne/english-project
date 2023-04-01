90b3fb66a337bc8f436efb92dd3958e04770488f
Aurélien <aurelien.troncy@telecomnancy.eu>
2023-04-01 15:08:57 +0200

    [+] ajout des fun_facts, et des mots aléatoires

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random as rd
 
 app = Flask(__name__)
 
@@ -9,3 +10,25 @@ def hello_world():
 @app.route('/lesson/<f>')
 def hello(f):
     return render_template('lessons/lesson' +f+ '.html')
+
+@app.route("/irregular_verbs")
+def irregular_verbs():
+    return render_template('irregular_verbs.html')
+
+
+@app.route("/fun_fact")
+def fun_fact():
+
+    with open("static/fun_facts") as f:
+        fun_facts = f.readlines()
+        fun_fact = fun_facts[rd.randint(0, len(fun_facts)-1)]
+    return render_template('fun_fact.html', fun_fact_content=fun_fact)
+
+
+@app.route("/random_word")
+def random_word():
+    with open("static/words") as f:
+        words = f.readlines()
+        random_word = words[rd.randint(0, len(words)-1)]
+        
+    return render_template('random_word.html', random_word_content=random_word)
