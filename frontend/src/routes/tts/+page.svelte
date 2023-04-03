024c05f57e158e1b2a30aa939c9a841346bc5d74
thibault-cne <thibault.chene23@gmail.com>
2023-04-03 16:36:56 +0200

    [+] add animation on irregular verbs

---
+++ frontend/src/routes/tts/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	const url = 'https://large-text-to-speech.p.rapidapi.com/tts';
+
+	const options = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'X-RapidAPI-Key': 'ef5f346a6amshe835ef743c94db9p10a9bfjsn401acd3872d0',
+			'X-RapidAPI-Host': 'large-text-to-speech.p.rapidapi.com'
+		},
+		body: '{"text":"Perfection is achieved not when there is nothing more to add, but rather when there is nothing more to take away."}'
+	};
+
+	fetch(url, options)
+		.then((res) => res.json())
+		.then((json) => console.log(json))
+		.catch((err) => console.error('error:' + err));
+</script>
