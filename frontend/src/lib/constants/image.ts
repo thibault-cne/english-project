ef7ba90dd6f09f3f122480298b4a0b54b60c03c2
thibault-cne <thibault.chene23@gmail.com>
2023-04-04 10:45:32 +0200

    [+] add hero page + center fun fact

--- frontend/src/lib/constants/image.ts
+++ frontend/src/lib/constants/image.ts
@@ -1,3 +1,5 @@
 import unionjack from '$lib/static/unionjack.jpeg';
+import thib from '$lib/static/F94C1ADE-0763-4C19-9F4E-C9F845993DF6 copy.jpg';
+import aurelien from '$lib/static/330926150_1633758117082753_2509878148082163560_n.jpg';
 
-export { unionjack };
+export { unionjack, thib, aurelien };
---
+++ frontend/src/routes/hero/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { thib, aurelien } from '$lib/constants/image';
+</script>
+
+<svelte:head>
+	<title>Hero</title>
+</svelte:head>
+
+<div class="flex w-full h-full justify-evenly items-center">
+	<div class="flex flex-col justify-center items-center">
+		<img src={aurelien} alt="Aurelien" class="rounded-full w-52" />
+		<span class="mt-4 font-bold text-lg">Aurelien T.</span>
+	</div>
+	<div class="flex flex-col justify-center items-center">
+		<img src={thib} alt="Thib" class="rounded-full w-52" />
+		<span class="mt-4 font-bold text-lg">Thibault C.</span>
+	</div>
+</div>
